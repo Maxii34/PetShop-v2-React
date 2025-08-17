3516fc6e20aa04e255a44104f74d2745174beecd
Maxi Ordoñez <exemaxi@outlook.com>
2025-08-17 19:08:34 -0300

    Se agrego un carousel con imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Navprimero from './Components/Navbar';
 import Navsegundo from './Components/SegundoNavbar';
-import './App.css'
 import Footer from './Components/Footer';
-
-
+import './App.css'
+import CarouselComponente from './Components/Carousel';
 
 
 function App() {
-
-
   return (
     <>
       <Navprimero />
       <Navsegundo />
       <main>
+      <CarouselComponente />
 
       </main>
       <Footer />
