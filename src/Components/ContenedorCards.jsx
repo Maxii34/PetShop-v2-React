4869b52af05e1cb3953019b076126fe8ba4e4338
Maxi Ordoñez <exemaxi@outlook.com>
2025-09-20 11:29:17 -0300

    Se mapeo el array de producto, y se renderizo las cards segun los productos cargados..

--- src/Components/ContenedorCards.jsx
+++ src/Components/ContenedorCards.jsx
@@ -1,23 +1,17 @@
-import { Container } from "react-bootstrap";
-import CardsCarousel from "./Productos/CardsCarousel";
+import CardsCarousel from "../components/Productos/CardsCarousel";
 
-
-const ContenedorCards = ({productosOferta}) => {
+const ContenedorCards = ({ productosOferta }) => {
   return (
-    <Container>
+    <>
       <h4 className="text-center fs-2 my-4">PROMOCIONES IMPERDIBLES 🎉</h4>
-      <div className="divContenedor-cards"
-      >
-        {productosOferta.map((producto) => (
-          <div
-            className="div-1"
-            key={producto.id}
-          >
-            <CardsCarousel productosOferta={productosOferta} />
+      <div className="divContenedor-cards">
+        {productosOferta.map((itemProducto, index) => (
+          <div className="div-1" key={index}>
+            <CardsCarousel itemProducto={itemProducto} />
           </div>
         ))}
       </div>
-    </Container>
+    </>
   );
 };
 
