afca36cc649bb1de2e3df2e7eac3849572807725
Maxi Ordoñez <exemaxi@outlook.com>
2025-09-21 08:06:39 -0300

    Se elimino texto del carousel baner

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -11,10 +11,6 @@ const CarouselComponente = () => {
           loading="lazy"
           style={{ height: "350px", objectFit: "cover" }}
         />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
@@ -24,10 +20,6 @@ const CarouselComponente = () => {
           loading="lazy"
           style={{ height: "350px", objectFit: "cover" }}
         />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
@@ -37,12 +29,6 @@ const CarouselComponente = () => {
           loading="lazy"
           style={{ height: "350px", objectFit: "cover" }}
         />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   );
