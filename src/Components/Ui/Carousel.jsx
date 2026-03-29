908d63507e885d7264897b9d6f12d7a02cfa1ccc
Maxi Ordoñez <exemaxi@outlook.com>
2026-03-29 12:38:29 -0300

    Agregado de baner para el carousel principal

--- src/Components/Ui/Carousel.jsx
+++ src/Components/Ui/Carousel.jsx
@@ -2,6 +2,9 @@ import { useEffect } from "react"; // 1. Faltaba importar esto
 import Carousel from "react-bootstrap/Carousel";
 import AOS from "aos"; 
 import "aos/dist/aos.css";
+import Baner01 from "/img/Baner01.png";
+import Baner02 from "/img/Baner02.png";
+import Baner03 from "/img/Baner03.png";
 
 export const CarouselComponente = () => {
   useEffect(() => {
@@ -12,33 +15,33 @@ export const CarouselComponente = () => {
 
   return (
     /* 2. Quitamos 'indice' y ponemos un número fijo o "0" */
-    <div data-aos="fade-right" data-aos-delay="600">
+    <div data-aos="fade-right" data-aos-delay="3000">
       <Carousel slide={false} className="shadow">
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="https://placedog.net/1000/400" 
+            src={Baner01}
             alt="First slide"
             loading="lazy"
-            style={{ height: "350px", objectFit: "cover" }}
+            style={{ height: "480px", objectFit: "cover", shadow: "lg" }}
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="https://placedog.net/1001/400"
+            src={Baner02}
             alt="Second slide"
             loading="lazy"
-            style={{ height: "350px", objectFit: "cover" }}
+            style={{ height: "480px", objectFit: "cover", shadow: "lg" }}
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="https://placedog.net/1002/400"
+            src={Baner03}
             alt="Third slide"
             loading="lazy"
-            style={{ height: "350px", objectFit: "cover" }}
+            style={{ height: "480px", objectFit: "cover", shadow: "lg" }}
           />
         </Carousel.Item>
       </Carousel>
